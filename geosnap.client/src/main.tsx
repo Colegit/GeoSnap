0a2a10279353ac834bedfe459551bc3786261a71
Cole Chapman <cchapman@rdffg.bc.ca>
2025-09-08 16:08:07 -0700

    Frontend: Added in tailwind themes

--- geosnap.client/src/main.tsx
+++ geosnap.client/src/main.tsx
@@ -5,6 +5,8 @@ import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <div data-theme="winter">
+      <App />
+    </div>
   </StrictMode>
 );
