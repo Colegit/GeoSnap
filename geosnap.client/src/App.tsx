6ea4404208289fbac3fd12eda62de2180b29356c
Cole Chapman <cchapman@rdffg.bc.ca>
2025-10-01 17:45:30 -0700

    Frontend: Switched to chakra ui as it made more sense for the app

--- geosnap.client/src/App.tsx
+++ geosnap.client/src/App.tsx
@@ -1,81 +1,23 @@
-import { useEffect, useState, type ReactElement } from "react";
+import { useEffect, type ReactElement } from "react";
 import "./App.css";
 import "react";
 import "./index.css";
+import { Button } from "@chakra-ui/react"
 
-interface Forecast {
+interface AppProps {
   date?: string | undefined;
   temperatureC?: number | undefined;
   temperatureF?: number | undefined;
   summary?: string | undefined;
 }
 
-const App = (props: Forecast): ReactElement => {
-  const [forecasts, setForecasts] = useState<Forecast[]>();
+const App = (props: AppProps): ReactElement => {
+  //const [forecasts, setForecasts] = useState<AppProps[]>();
 
-  useEffect(() => {
-    populateWeatherData();
-  }, []);
-
-  const contents =
-    forecasts === undefined ? (
-      <p>
-        <em>
-          Loading... Please refresh once the ASP.NET backend has started. See{" "}
-          <a href="https://aka.ms/jspsintegrationreact">
-            https://aka.ms/jspsintegrationreact
-          </a>{" "}
-          for more details.
-        </em>
-      </p>
-    ) : (
-      <table className="table table-striped" aria-labelledby="tableLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map((forecast) => (
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    );
 
   return (
-    <div>
-      <h1 className="text-2xl font-bold text-blue-600" id="tableLabel">
-        Weather forecast
-      </h1>
-      <p>
-        This component demonstrates fetching data from the server nice{" "}
-        {props.date}
-      </p>
-      {contents}
-
-      <div className="p-10">
-        <h1 className="text-2xl font-bold mb-4">DaisyUI Test</h1>
-        <button className="btn btn-soft btn-primary">Click Me</button>
-      </div>
-    </div>
+      <Button>click me chakra { props.date}</Button>
   );
-
-  async function populateWeatherData() {
-    const response = await fetch("weatherforecast");
-    if (response.ok) {
-      const data = await response.json();
-      setForecasts(data);
-    }
-  }
 };
 
 export default App;
--- geosnap.client/src/main.tsx
+++ geosnap.client/src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
+import { Provider } from "@/Components/ui/provider";
 import App from "./App.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <div data-theme="winter">
-      <App />
-    </div>
-  </StrictMode>
+    <StrictMode>
+        <Provider>
+            <App />
+        </Provider>
+    </StrictMode>
 );
